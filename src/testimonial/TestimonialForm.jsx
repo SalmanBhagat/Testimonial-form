828f0123bdc54558122512aa1030fbe7f5589917
SalmanBhagat <mrsalman9222@gmail.com>
2025-07-28 20:33:06 +0530

    update code

--- src/testimonial/TestimonialForm.jsx
+++ src/testimonial/TestimonialForm.jsx
@@ -1,7 +1,8 @@
 import styles from "./testimonialForm.module.css";
-import z from "zod";
+import z, { includes } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
 
 const schema = z.object({
   name: z.string().min(1, { error: "Enter Your Name" }),
@@ -13,6 +14,9 @@ const schema = z.object({
 });
 
 function TestimonialForm() {
+
+  const [selectedImg, setSelectedImg] = useState("");
+
   const {
     register,
     handleSubmit,
@@ -25,7 +29,24 @@ function TestimonialForm() {
   };
 
   const handleImage = (input) => {
-    setValue("image", input.target.files[0]);
+    const File = input.target.files[0];
+
+    const fileTypes = ["image/png","image/jpeg","image/gif"];
+
+    const fileType = File.type
+    
+    if (fileTypes.includes(fileType)) {
+      
+      setSelectedImg(URL.createObjectURL(File))
+      setValue("image", File);
+
+    }
+    else{
+      window.alert("Please Selected : Png, Jpeg, Gif Image File Type");
+    }
+
+
+
     console.log(input.target.files[0]);
   };
 
@@ -51,7 +72,12 @@ function TestimonialForm() {
         </div>
         <div className="mb-3">
           <label className="form-label">Click to upload</label>
-          <input type="file" className="form-control" onChange={handleImage} />
+          <div className="d-flex justify-content-between align-items-start">
+            <input type="file" className="form-control w-50" onChange={handleImage} />
+          {selectedImg && (
+            <img style={{objectFit: "contain", height: "100%", borderRadius: "6px"}} src={selectedImg} alt="Img" height={200} width={300}/>
+          )}
+          </div>
           {errors.image && <p>{errors.image.error}</p>}
         </div>
         <div className="mb-3 form-check">
